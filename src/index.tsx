3bf13e3ae16da19608e16109f075467cd96bfba6
Gabriel <gabriel.costa513@gmail.com>
2022-06-17 10:59:41 -0300

    criando pastas de componentes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom';
 import App from './App'
+import {GlobalStyle} from './globalStyle'
+
 
 
 ReactDOM.render(
-  <App />,
+  <>
+  <GlobalStyle/>
+    <App />
+  </>,
   document.getElementById('root')
 
 )
