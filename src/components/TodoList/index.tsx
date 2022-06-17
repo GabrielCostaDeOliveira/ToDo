a4272e8ec0ed60697c506f99605e06d81932d380
Gabriel <gabriel@Cartola.localdomain>
2022-06-17 16:02:04 -0300

    adicionando updataToDo e estilo

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import TodoForm from "../TodoForm";
 import { RiCloseCircleFill } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
+import { TodoRow, DivIcons } from './styled'
 
 interface todo{
     id:number,
@@ -29,51 +31,38 @@ class TodoList extends React.Component<Props,Stage> {
         }
     }
 
-    handleSubmitUpdate(event: React.FormEvent<HTMLFormElement>){
-        event.preventDefault()
-        this.setState({showInputUpdade:false});
 
-        this.props.updateTodo(this.state.editTodo)
+    handleSubmit(newstring:string) {
+        var e:todo = {id : this.state.editTodo.id, text: newstring }
+
+        this.props.updateTodo(e)
 
         var e:todo = {id : 0, text: "" }
 
-        this.setState({editTodo: e});
-    }
-
-
-    handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-        var e:todo = {id : this.state.editTodo.id, text: event.target.value }
-
-        this.setState({editTodo: e});
+        this.setState({editTodo: e, showInputUpdade:false});
     }
 
     render(){
         if (this.state.showInputUpdade)
-            return (
-                <form onSubmit={this.handleSubmitUpdate.bind(this)}>
-                    <input type="text" placeholder="Digite a nova tarefa" value={this.state.editTodo.text} onChange={this.handleChange.bind(this)} /> 
-                    <button>Ok</button>
-                </form>
-            )
-
+            return <TodoForm  type={"e"} submit={this.handleSubmit.bind(this)} value={this.state.editTodo.text}/>
 
         return (
             <>
                 {this.props.todos.map(
                         (todo : todo) => (
-                            <div key={todo.id.toString()}>
+                            <TodoRow  typeColor={todo.id} key={todo.id.toString()} >
                                 <div > 
                                     {todo.text}
                                 </div>
 
-                                <div>
+                                <DivIcons>
                                     <RiCloseCircleFill  onClick={ () => this.props.removeTodo(todo.id)}/>
                                     <TiEdit onClick={ () => { 
                                         this.setState({showInputUpdade : true, editTodo:todo})
                                         }
                                     }/>
-                                </div>
-                            </div>
+                                </DivIcons>
+                            </TodoRow>
                         )
                     )
                 }
